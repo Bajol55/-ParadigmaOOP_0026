2b0c391bf0c5e86401c89659afe79e71318a45ad
Bajol55 <devarafadil35@gmail.com>
2026-04-20 10:39:40 +0700

    2

--- Paradigma.cpp
+++ Paradigma.cpp
@@ -1,3 +1,26 @@
 #include <iostream>
 using namespace std;
 
+class mahasiswa {
+
+    public :
+    int nim;
+    string nama;
+    float nilai ;
+    
+    void prinData() {
+        cout << nim << endl;
+        cout << nama << endl;
+        cout << nilai << endl;
+
+    }
+}]
+int main () {
+    Mahasiswa mhs;
+    mhs.nim = 12345;
+    mhs.nama = "John Doe"
+    mhs.nilai = 85,5;
+    mhs.printData();
+}
+    }
+}
