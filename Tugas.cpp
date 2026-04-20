d228075e427928d995fc449f0ae927d7df40c981
Bajol55 <devarafadil35@gmail.com>
2026-04-20 11:22:50 +0700

    4

--- Tugas.cpp
+++ Tugas.cpp
@@ -7,3 +7,36 @@ public:
     int jumlah;
     string kategori;
     string tanggalProduksi;
+
+    // Method untuk menampilkan data
+    void tampilkanData() {
+        cout << "Nama Barang      : " << nama << endl;
+        cout << "Jumlah           : " << jumlah << endl;
+        cout << "Kategori         : " << kategori << endl;
+        cout << "Tanggal Produksi : " << tanggalProduksi << endl;
+        cout << "-----------------------------" << endl;
+    }
+};
+
+int main() {
+    Barang elektronik;
+    Barang nonElektronik;
+
+    // Isi data object elektronik
+    elektronik.nama = "Laptop";
+    elektronik.jumlah = 10;
+    elektronik.kategori = "Elektronik";
+    elektronik.tanggalProduksi = "2026-03-01";
+
+    // Isi data object nonElektronik
+    nonElektronik.nama = "Buku";
+    nonElektronik.jumlah = 25;
+    nonElektronik.kategori = "Non Elektronik";
+    nonElektronik.tanggalProduksi = "2026-01-15";
+
+    // Tampilkan data
+    elektronik.tampilkanData();
+    nonElektronik.tampilkanData();
+
+    return 0;
+}
